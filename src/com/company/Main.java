4fc88203d52aa47ef85e9d160803ab9d28c1a11f
oliverbark <oliver.bark@icloud.com>
2021-09-14 14:13:50 +0200

    Now listToNumbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,7 +17,6 @@ public class Main {
 
     public static int[] textToListOfNumbers(String text){
         int [] numbers = new int[text.length()];
-
         char[] letters = text.toCharArray();
 
         for(int i = 0; i < text.length(); i++) {
@@ -28,20 +27,7 @@ public class Main {
     }
 
     public static int letterToNumber(char letter) {
-/*        char[] alfabet = {'A','B','C','D','E'};
-        int[]  numre   = { 1,  2,  3,  4,  5 };
 
-        int nummer = 0;
-        for(int i = 0; i< alfabet.length; i++){
-            char bogstavetViTjekker = alfabet[i];
-            if( letter == bogstavetViTjekker){
-                System.out.printf("Fundet bogstav %c på index %d", bogstavetViTjekker, i);
-                nummer = i;
-                //nummer = numre[i]
-                System.out.printf("og det har nummer: ∞d\n", nummer);
-            }
-        }
-*/
         String alfabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
         int num = alfabet.indexOf( letter );
         return num;
